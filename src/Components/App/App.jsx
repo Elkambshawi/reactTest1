c914a802bedadb8b0bf3e03a23023ce011ee28c3
Abdelrahman Elkambshawi <elnegmabdo@gmail.com>
2023-09-11 00:23:38 +0300

    update

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -13,37 +13,25 @@ import { store } from '../Redux/store';
 import Cart from '../Cart/Cart';
 import User from '../User/User';
 import Contact from '../Contact/Contact';
-const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element = {<Layout />}>
-        <Route index element = {<Home />} />
-        <Route path="shop" element = {<Shop />} />
-        <Route path="about" element = {<About />} />
-        <Route path="contact" element = {<Contact />} />
-        <Route path="details/:id" element = {<Details />} />
-        <Route path="cart" element = {<Cart />} />
-        <Route path="user" element = {<User />} />
-      </Route>
-    )
-  )
+
+const router = createBrowserRouter([
+
+  {path:"/",element: <Layout />,
+    children: [
+      {index:true,element:<Home/>},
+      {path:"shop",element:<Shop/> },
+      {path:"about",element:<About/> },
+      {path:"contact",element:<Contact/> },
+      {path:"details/:id", element: <Details /> },
+      {path:"cart", element: <Cart/> },
+      {path:"user", element: <User/> },
+
+      ]
+
+    },
+])
 function App() {
   
-  // const router = createBrowserRouter([
-
-  //   {path:"/",element: <Layout />,
-  //     children: [
-  //       {index:true,element:<Home/>},
-  //       {path:"shop",element:<Shop/> },
-  //       {path:"about",element:<About/> },
-  //       {path:"contact",element:<Contact/> },
-  //       {path:"details/:id", element: <Details /> },
-  //       {path:"cart", element: <Cart/> },
-  //       {path:"user", element: <User/> },
-
-  //       ]
-
-  //     },
-  // ])
 
   return (
     <>
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import Navbar from '../Navbar/Navbar'
 export default function Layout() {
   return (
     <>
-      <h2>Hello World 3</h2>
+      <Navbar/>
       <Outlet />
     </>
   )
